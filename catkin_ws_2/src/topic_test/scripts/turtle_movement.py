ef8b56004c7f9946a1635e181e12c64dbfcef31a
AnishPawar <anish.pawar@somaiya.edu>
2020-08-03 20:36:31 +0530

    [turtle mover node]

--- catkin_ws_2/src/topic_test/scripts/turtle_movement.py
+++ catkin_ws_2/src/topic_test/scripts/turtle_movement.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Float64MultiArray
+from geometry_msgs.msg import Twist
 
 def mover():
-    pub = rospy.Publisher('/turtle1/cmd_vel',Float64MultiArray,queue_size= 10)
+    pub = rospy.Publisher('/turtle1/cmd_vel',Twist,queue_size= 10)
     rospy.init_node('mover',anonymous= True)
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
-        vel = [2.0, 0.0, 0.0] [0.0, 0.0, 1.8]
+        vel = Twist()
+        
+        vel.angular.x = 0.0
+        vel.angular.y = 0.0
+        vel.angular.z = 1.8
+
+        vel.linear.x = 2.0
+        vel.linear.y = 0.0
+        vel.linear.z = 0.0
+
         rospy.loginfo(vel)
         pub.publish(vel)
         rate.sleep()
